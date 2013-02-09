32db855a75edc703ad30919e6be5cf246c4c4236
haraacharya <hara.acharya@gmail.com>
2013-02-08 18:52:58 -0800

    maildomain change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "localhost:3000",
+  :domain               => "http://sleepy-lowlands-4295.herokuapp.com",
   :user_name            => "hara.acharya",
   :password             => "Agrima2012@",
   :authentication       => "plain",
