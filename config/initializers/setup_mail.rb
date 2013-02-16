b023045e58a8e03a41065ce9f55b0ec79fa92028
haraacharya <hara.acharya@gmail.com>
2013-02-15 17:35:17 -0800

    changed environment file for deployment

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "http://localhost:3000",
+  :domain               => "http://mailme-nosreves.herokuapp.com",
   :user_name            => "donotreply.mailme@gmail.com",
   :password             => "Bekham@2",
   :authentication       => "plain",
